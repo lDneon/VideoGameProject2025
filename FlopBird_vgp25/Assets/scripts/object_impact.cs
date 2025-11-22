74ddf098c3a4dcec324c64a7d6c889035c097807
lDneon <debreuslovemica@gmail.com>
2025-11-22 18:50:52 -0500

    FlopBird continue
    need to work on the impact scripts. Need scoring record and health progress

---
+++ FlopBird_vgp25/Assets/scripts/object_impact.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class object_impact : MonoBehaviour
+{
+    public GameObject stickPrefab; //these two should respond to the tags given to each prefab in the unity Hierachy!
+    public GameObject foodPrefab;
+
+    public float stickInterval = 1f;
+    public float foodInterval = 5f;
+
+    public float spawnY = 6f;
+    public float minX = -3f;
+    public float maxX = 3f;
+
+    void stickSpawn()
+    {
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), spawnY, 0f);
+        Instantiate(stickPrefab, pos, Quaternion.identity);
+    }
+    void foodSpawn()
+    {
+        Vector3 pos = new Vector3(Random.Range(minX, maxX), spawnY, 0f);
+        Instantiate(foodPrefab, pos, Quaternion.identity);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(stickSpawn), 1f, stickInterval);
+        InvokeRepeating(nameof(foodSpawn), 2f, foodInterval);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
